6d77a9cfb9565f708ad25cfe54c15bc4b2a797db
jason-bourne-gg <car10@iitbbs.ac.in>
2023-03-27 19:32:04 +0530

    added app.js and index .js code and installed MUI libs

--- react-redux-form/src/App.js
+++ react-redux-form/src/App.js
@@ -1,7 +1,27 @@
+// import Form from "./components/Form";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1f2937",
+      white: "#ffffff",
+    },
+    text: {
+      primary: "#173A5E",
+      secondary: "#46505A",
+    },
+  },
+});
 
 function App() {
   return (
-    <p>Hi</p>
+    <CssBaseline>
+      <ThemeProvider theme={theme}>
+        <Form />
+      </ThemeProvider>
+    </CssBaseline>
+    
   );
 }
 
